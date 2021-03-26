7b09b33370816f33a9f42a5090432a44f5aef40e
Amorésio <a.junior.srlopes@gmail.com>
2021-03-26 01:43:07 -0400

    add: github client

---
+++ lib/brainn/github_api/client.ex
@@ -0,0 +1,18 @@
+defmodule Brainn.GithubApi.Client do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://api.github.com"
+  plug Tesla.Middleware.Headers, [{"User-Agent", "brainn"}]
+
+  plug Tesla.Middleware.JSON
+
+  def get_starred_repos_by_username(user) do
+    "/users/#{user}/starred"
+    |> get()
+    |> handle_get()
+  end
+
+  defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_get({:ok, %Tesla.Env{status: 404}}), do: {:error, "User not found"}
+  defp handle_get({:error, _reason} = error), do: error
+end
