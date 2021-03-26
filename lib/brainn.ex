9bd0fe847dd515691f157328c61b695e4a6f61f4
Amorésio <a.junior.srlopes@gmail.com>
2021-03-26 01:36:07 -0400

    feat: add facade to fetch starred repositories

--- lib/brainn.ex
+++ lib/brainn.ex
@@ -1,9 +1,5 @@
 defmodule Brainn do
-  @moduledoc """
-  Brainn keeps the contexts that define your domain
-  and business logic.
+  alias Brainn.Repositories
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate fetch_starred_repositories(params), to: Repositories.Get, as: :call
 end
