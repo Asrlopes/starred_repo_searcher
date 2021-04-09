202a196a3ca3c0dd0ac69df3ae1143841b216c49
asrlopes <a.junior.srlopes@gmail.com>
2021-04-09 10:25:49 -0400

    add module to update a tag

---
+++ lib/brainn/tags/update.ex
@@ -0,0 +1,35 @@
+defmodule Brainn.Tags.Update do
+  alias Brainn.Repo
+  alias Brainn.Tags
+
+  def call(params) do
+    params
+    |> handle_update()
+  end
+
+  defp handle_update(%{"id" => id} = params) do
+    with {:ok, %Tags{} = tag} <- get_tags!(id),
+         {:ok, %Tags{} = tag} <- update_tag(tag, params) do
+          {:ok, tag}
+    end
+  end
+
+  def get_tags!(id) do
+    {:ok, Repo.get!(Tags, id)}
+  rescue
+    Ecto.NoResultsError -> {:error, :not_found}
+  end
+
+  def update_tag(%Tags{} = tag, attrs) do
+    case Tags.update_changeset(attrs) do
+      %Ecto.Changeset{valid?: true} ->
+        tag
+        |> Repo.preload([:starred_repos])
+        |> Tags.changeset(attrs)
+        |> Repo.update()
+
+      e ->
+        {:error, e}
+    end
+  end
+end
