00fd2075e9fe900dc3b41ec208205ed39579c2fb
asrlopes <a.junior.srlopes@gmail.com>
2021-04-09 10:05:24 -0400

    add facade to create and update tags

--- lib/brainn.ex
+++ lib/brainn.ex
@@ -1,5 +1,8 @@
 defmodule Brainn do
-  alias Brainn.Repositories
+  alias Brainn.{Repositories, Tags}
 
   defdelegate fetch_starred_repositories(params), to: Repositories.Get, as: :call
+
+  defdelegate create_tags(params), to: Tags.Create, as: :call
+  defdelegate update_tags(params), to: Tags.Update, as: :call
 end
