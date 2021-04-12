aa908c346783922e32ca0e17a691bfc8d0d14718
asrlopes <a.junior.srlopes@gmail.com>
2021-04-12 09:26:14 -0400

    fix: unique constraint

--- lib/brainn/tags.ex
+++ lib/brainn/tags.ex
@@ -13,11 +13,13 @@ defmodule Brainn.Tags do
     tag
     |> cast(attrs, [:name])
     |> validate_required([:name])
+    |> unique_constraint(:name)
   end
 
   def update_changeset(attrs) do
     %__MODULE__{}
     |> cast(attrs, [:name])
     |> validate_required([:name])
+    |> unique_constraint(:name)
   end
 end
