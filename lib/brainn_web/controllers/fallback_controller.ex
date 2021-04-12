589f3f903673ea272129f616982ada17844193c2
asrlopes <a.junior.srlopes@gmail.com>
2021-04-12 09:28:18 -0400

    add not found handler

--- lib/brainn_web/controllers/fallback_controller.ex
+++ lib/brainn_web/controllers/fallback_controller.ex
@@ -1,6 +1,13 @@
 defmodule BrainnWeb.FallbackController do
   use BrainnWeb, :controller
 
+  def call(conn, {:error, "Resource not found"}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(BrainnWeb.ErrorView)
+    |> render("404.json", result: "Resource not found")
+  end
+
   def call(conn, {:error, result}) do
     conn
     |> put_status(:bad_request)
