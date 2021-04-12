a45d1ea14ac63e09ba39d23a02b8c766db86db5a
asrlopes <a.junior.srlopes@gmail.com>
2021-04-12 09:27:19 -0400

    add traverse errors

--- lib/brainn_web/views/error_view.ex
+++ lib/brainn_web/views/error_view.ex
@@ -1,6 +1,8 @@
 defmodule BrainnWeb.ErrorView do
   use BrainnWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -12,5 +14,21 @@ defmodule BrainnWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: message}) do
+    %{message: translate_erros(message)}
+  end
+
+  def render("404.json", %{result: message}) do
+    %{message: message}
+  end
+
+  defp translate_erros(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
